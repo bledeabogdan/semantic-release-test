b09620176a11336a1a6c0ec2b378d4debf07d2ec
bledeabogdan <bogdan.bledea@ipsos.com>
2022-12-05 16:21:23 +0200

    chore: first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+export function add(a, b) {
+  return a + b;
+}
+
+export function subtract(a, b) {
+  return a - b;
+}
+
+export function division(a, b) {
+  if (b !== 0) return a / b;
+
+  return undefined;
+}
+
+export function product(a, b) {
+  return a * b;
+}
